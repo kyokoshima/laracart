3245b4d01a77aea92806d9179973efcce69b4c92
kyokoshima <dev.kyoko@gmail.com>
2014-08-07 13:41:07 +0000

    change thumnail size

--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<div class="row product-list">
 	@foreach ($products as $product)
-		<div class="col-xs-6 col-md-3">
+		<div class="col-xs-6">
 			<div class="thumbnail">
-				<img src="holder.js/100x100" alt="">
+				<img src="holder.js/200x200" alt="">
 				{{ $product->name }}
 			</div>
 		</div>
